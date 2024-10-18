98b0e67b3201016ea24d021257e19274c6ccd4ea
Chase Bugeaud <cbugeaud@shaw.ca>
2024-10-18 16:22:20 -0700

    updated Order.java addItem() function

--- Order.java
+++ Order.java
@@ -4,26 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    private List<MenuItem> items = new ArrayList<>();
-    
-    public void addItem(MenuItem item) {
-        items.add(item);
-    }
-    
-    public double calculateTotal() {
-        double total = 0;
-        for (MenuItem item : items) {
-            total += item.getPrice();
-        }
-        return total;
-    }
+  private List<MenuItem> items = new ArrayList<>();
 
-    public void printOrderDetails() {
-        System.out.println("Order Details:");
-        for (MenuItem item : items) {
-            System.out.println("- " + item.getName() + ": $" + item.getPrice());
-        }
-        System.out.println("Total: $" + calculateTotal());
+  public void addItem(MenuItem item) {
+    System.out.println("Item " + item.getName() + "added");
+    items.add(item);
+  }
+
+  public double calculateTotal() {
+    double total = 0;
+    for (MenuItem item : items) {
+      total += item.getPrice();
     }
+    return total;
+  }
+
+  public void printOrderDetails() {
+    System.out.println("Order Details:");
+    for (MenuItem item : items) {
+      System.out.println("- " + item.getName() + ": $" + item.getPrice());
+    }
+    System.out.println("Total: $" + calculateTotal());
+  }
 }
-
