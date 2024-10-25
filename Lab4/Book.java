009a4027acaf9ade4cd12750d5928b28127d04b1
Chase Bugeaud <cbugeaud@shaw.ca>
2024-10-25 16:19:25 -0700

    finished book

--- Lab4/Book.java
+++ Lab4/Book.java
@@ -1,38 +1,38 @@
 package Lab4;
- 
+
 public class Book {
 
-	// GRASP Principle: Information Expert
-	// Manages its own state (available or borrowed)
-	// TODO: implement Book functionality 
-	
-	//Variables
-	private String title;
-	private boolean isAvailable; 
-	private String author;
-	
-	//constructor
-	public Book(String title, String author, boolean isAvailable) {
-		this.title = title;
-		this.author = author;
-		this.isAvailable = isAvailable;
-	}
-	
-	//set methods
-	public void setIsAvailable(boolean newAvailability) {
-		this.isAvailable = newAvailability;
-	}
-	
-	//Get methods
-	public boolean getIsAvailable() {
-		return this.isAvailable;
-	}
-	
-	public String getTitle() {
-		return this.title;
-	}
-	
-	public String getAuthor() {
-		return this.author;
-	}
+  // GRASP Principle: Information Expert
+  // Manages its own state (available or borrowed)
+  // DONE: implement Book functionality
+
+  // Variables
+  private String title;
+  private boolean isAvailable;
+  private String author;
+
+  // constructor
+  public Book(String title, String author, boolean isAvailable) {
+    this.title = title;
+    this.author = author;
+    this.isAvailable = isAvailable;
+  }
+
+  // set methods
+  public void setIsAvailable(boolean newAvailability) {
+    this.isAvailable = newAvailability;
+  }
+
+  // Get methods
+  public boolean getIsAvailable() {
+    return this.isAvailable;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public String getAuthor() {
+    return this.author;
+  }
 }
