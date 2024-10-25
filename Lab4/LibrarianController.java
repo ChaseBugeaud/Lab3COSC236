eae80679df9f051bf6ab099b489d5790490a3666
Himeeka <Goodlandliam@gmail.com>
2024-10-25 15:36:38 -0700

    Added lab 4 files

---
+++ Lab4/LibrarianController.java
@@ -0,0 +1,11 @@
+package Lab4;
+
+public class LibrarianController {
+
+	// GRASP Principle: Controller
+	// Handles user requests like borrowing and returning books.
+	// Delegates tasks to the appropriate objects.
+	
+	// TODO: implement functionality of Member class
+
+}
