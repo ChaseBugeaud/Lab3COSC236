803057bc99eebde81b93c99132d2e2aabc29d90c
KIverson11 <kiverson11@gmail.com>
2024-10-25 16:35:11 -0700

    *stand on table* I HAVE MADE CHANGES TO LIBRARY MEMBER

--- Lab4/Member.java
+++ Lab4/Member.java
@@ -1,11 +1,34 @@
 package Lab4;
 
+import java.util.ArrayList;
+
 public class Member {
 
 	// GRASP Principle: Information Expert and Low Coupling
 	// Can borrow and return books.
 
 	private String name;
+	private String memberId;
+	ArrayList<Book> borrowedBooks = new ArrayList<>();
+	
+	public Member(String name, String memberId) {
+		super();
+		this.name = name;
+		this.memberId = memberId;
+	}
+	public void addBook(Book name){
+		borrowedBooks.add(name);
+	}
+	public void returnBook(Book name) {
+		borrowedBooks.remove(name);
+	}
+	public String getName() {
+		return name;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	
 	// private borrowedbBooks TODO: implement collection of borrowed books
 	
 	// TODO: implement functionality of Member class
