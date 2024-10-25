3c808eee5709218d9b5f2c83c3dcc7fb7e081543
Chase Bugeaud <cbugeaud@shaw.ca>
2024-10-25 16:52:04 -0700

    started LibrarianController

--- Lab4/LibrarianController.java
+++ Lab4/LibrarianController.java
@@ -2,10 +2,17 @@ package Lab4;
 
 public class LibrarianController {
 
-	// GRASP Principle: Controller
-	// Handles user requests like borrowing and returning books.
-	// Delegates tasks to the appropriate objects.
-	
-	// TODO: implement functionality of Member class
+  // GRASP Principle: Controller
+  // Handles user requests like borrowing and returning books.
+  // Delegates tasks to the appropriate objects.
 
+  // TODO: implement functionality of Member class
+
+  public void returnBook(Book book, Member member) {
+    // TODO: complete this function
+  }
+
+  public void borrowBook(Book book, Member member) {
+    // TODO: complete this function
+  }
 }
