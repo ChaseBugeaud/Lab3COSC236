f7c5e0336bd2fde7850f9873d09cea976d25e8e6
Himeeka <Goodlandliam@gmail.com>
2024-11-08 15:30:43 -0800

    Working on LibrarianController

--- Lab4/LibrarianController.java
+++ Lab4/LibrarianController.java
@@ -7,12 +7,12 @@ public class LibrarianController {
   // Delegates tasks to the appropriate objects.
 
   // TODO: implement functionality of Member class
-
-  public void returnBook(Book book, Member member) {
-    // TODO: complete this function
-  }
-
-  public void borrowBook(Book book, Member member) {
-    // TODO: complete this function
-  }
+	
+	Library library;
+	
+	
+	public LibrarianController(Library library) {
+		this.library = library;
+	}
+	
 }
