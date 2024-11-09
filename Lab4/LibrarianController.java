bc9d657fe44d07b9fa52deca2c0e8ba20e1752ae
Chase Bugeaud <cbugeaud@shaw.ca>
2024-11-08 16:37:41 -0800

    removed unnecessary lines in librariancontroller.java

--- Lab4/LibrarianController.java
+++ Lab4/LibrarianController.java
@@ -6,41 +6,35 @@ public class LibrarianController {
   // Handles user requests like borrowing and returning books.
   // Delegates tasks to the appropriate objects.
 
-  // TODO: implement functionality of Member class
-
-  // TODO: implement functionality of Member class
-
   Library library;
 
   public LibrarianController(Library library) {
     this.library = library;
   }
-  
+
   public void addBookToLibrary(String title, String author) {
-	  this.library.createBook(title, author);
+    this.library.createBook(title, author);
   }
-  
+
   public void addMemberToLibrary(String name) {
-	  this.library.createMember(name);
+    this.library.createMember(name);
   }
-  
-  
+
   public void rentBook(String bookName, String memberName) {
-	  this.library.rentBook(bookName, memberName);
-	  
+    this.library.rentBook(bookName, memberName);
+
   }
-  
+
   public void returnBook(String bookName, String memberName) {
-	  this.library.returnBook(bookName, memberName);
+    this.library.returnBook(bookName, memberName);
   }
-  
-  
+
   public void showAllBooks() {
-	  library.showBooks();
+    library.showBooks();
   }
-  
+
   public void showAllMembers() {
-	  library.showMembers();  
+    library.showMembers();
   }
-  
+
 }
